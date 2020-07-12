8354190c9b0351ca8143f72e93d6ca7b70412c75
nabinpantha <panthanabin@gmail.com>
2020-07-12 15:03:11 -0500

    first commit

---
+++ src/Form.jsx
@@ -0,0 +1,11 @@
+import React , {useState} from 'react'
+
+
+export default  ()=> {
+
+    const [value , setValue] = useState("");
+
+    return <input value = {value} onChange = { e =>setValue(e.target.value)} />
+        
+    
+}
