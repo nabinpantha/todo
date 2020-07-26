9de2ec4b60dd6336f00d1233a3714ddf0ca3d39c
nabinpantha <panthanabin@gmail.com>
2020-07-26 14:55:05 -0500

    update Balance component dynamically

--- src/component/Balance.js
+++ src/component/Balance.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalContext} from '../Context/GlobalState'
 
 const Balance = () => {
+
+    const {incomeTransactions, expenseTransactions} = useContext(GlobalContext);
+    
+    const incomeAmounts = incomeTransactions.map(
+        incomeTransaction => incomeTransaction.incomeAmount
+    )
+
+    const totalIncome = incomeAmounts.reduce((prev, current) => (prev += current),0).toFixed(2);
+    
+ const expenseAmounts = expenseTransactions.map(expenseTransaction => expenseTransaction.expenseAmount)
+
+const totalExpense = expenseAmounts.reduce((prev, current)=>prev += current,0).toFixed(2);
+
+//const totalBalance = (totalIncome *1 ) - (totalExpense *1);
     return (
         <div className= "balance"> 
             <h2>Balance</h2>
-            <h3>$0.98</h3>
+            <h3>{(totalIncome *1 ) - (totalExpense *1)}</h3>
             <div className= "income-expense">
                 <div className="plus">
                 <h3>income</h3>
-                <p>+$0.98</p>
+                <p>+{totalIncome}</p>
                 </div>
                 <div className="minus">
                 <h3>expense</h3>
-                <p>- $0.98</p>
+                <p>-{totalExpense}</p>
                 </div>
             </div>
         </div>
